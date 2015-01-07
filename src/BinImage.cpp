8bb2ad02e16592501ed6d3e3e9ba952a3656d537
Erik Timmers <e.timmers@gmail.com>
2015-01-07 09:36:33 +0100

    Read image dimensions

--- src/BinImage.cpp
+++ src/BinImage.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "BinImage.h"
@@ -21,4 +23,25 @@ void BinImage::create_png(void)
     if (strcmp(_data, "MotoRun")) {
         cerr << "Could not find valid image data" << endl;
     }
+    _g = 8; // skip 8 bytes
+
+    // read image dimensions
+    _width = _read_value();
+    _height = _read_value();
+
+    if (!_width || !_height) {
+        cerr << "Could not determine image size" << endl;
+    }
+    cout << "Image size: " << _width << " x " << _height << endl;
+}
+
+size_t BinImage::_read_value(void) {
+    char bytes[2];
+    bytes[0] = _data[_g++];
+    bytes[1] = _data[_g++];
+    size_t value = 0;
+    for (int i=0; i<2; i++) {
+        value += pow(256, 1 - i) * (uint8_t)bytes[i];
+    }
+    return value;
 }
--- src/BinImage.h
+++ src/BinImage.h
@@ -11,10 +11,13 @@ public:
     void create_png(void);
 
 private:
+    size_t _read_value(void);
+
     size_t _size;
     size_t _width;
     size_t _height;
     char *_data;
+    int _g;
 };
 
 #endif
